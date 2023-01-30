920460352ae8cb9da6b3e3c9e434d9f612f9a082
Laura Muntjewerf <laura.muntjewerf@knmi.nl>
2023-01-30 14:54:11 +0000

    re-name and re-order files

---
+++ 01_preprocessing/initial_conditions/setup_ic_folder_structure/01a_untar_ics.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+set -ex
+
+#-------------------
+# Untar all the ic folders from the Bull
+#
+# The script assumes the tar-files are in $SCRATCH
+#
+# x  instructs tar to extract the files from the zipped file
+# v  means verbose, or to list out the files it's extracting
+# z  instructs tar to decompress the files without this, you'd have a folder full of compressed files
+# f  tells tar the filename you want it to work on
+#
+# Written by Laura Muntjewerf 
+#   laura.muntjewerf@gmail.com
+#-------------------
+
+
+
+scenario=historical
+for j in {01..16}; do
+  tarfile=$SCRATCH/"$scenario"/"$scenario"-"$(printf "%02d" $j)"/20000101.tar.gz
+  dest=$SCRATCH/"$scenario"/"$scenario"-"$(printf "%02d" $j)"/20000101
+  mkdir -p $dest
+  tar -xvzf $tarfile -C $dest/.
+done
+
+scenario=ssp2-4.5
+for j in {15..16}; do
+  tarfile=$SCRATCH/"$scenario"/"$scenario"-"$(printf "%02d" $j)"/20750101.tar.gz
+  dest=$SCRATCH/"$scenario"/"$scenario"-"$(printf "%02d" $j)"/20750101
+  mkdir -p $dest
+  tar -xvzf $tarfile -C $dest/.
+done
+
+
+
+
---
+++ 01_preprocessing/initial_conditions/setup_ic_folder_structure/01b_untar_only_nemo.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+set -ex
+
+#-------------------
+# Untar only nemo files (user needs to specify the location of the folder)
+# Because the way it was saved, it is necessary do to some stripping of the folder structure
+#
+# # Written by Laura Muntjewerf 
+#   laura.muntjewerf@gmail.com
+#-------------------
+
+
+# make it for the historical set
+declare -a scenarios
+scenarios=(h)
+
+for scenario in ${scenarios[@]}; do
+
+  for ic_member in {03..16}; do
+    member=$(printf "%02d" $ic_member)
+    scenario1=h
+    scenario_short=ha
+    scenario_full=historical
+    date=20000101
+    prevdate=199912
+    folder=
+
+    cd /"$folder"/nemo/
+    tar -xvf ../../"$date".tar.gz "$folder"/nemo/  --strip-components 19
+
+  done
+done
+
+# make it for the scenario set
+declare -a scenarios
+scenarios=(s)
+
+for scenario in ${scenarios[@]}; do
+
+  for ic_member in {15..16}; do
+    member=$(printf "%02d" $ic_member)
+    scenario1=s
+    scenario_short=s2
+    scenario_full=ssp2-4.5
+    date=20750101
+    prevdate=207412
+    folder=
+
+    cd /"$folder"/nemo/
+    tar -xvf ../../"$date".tar.gz "$folder"/nemo/  --strip-components 19
+
+  done
+done
+
